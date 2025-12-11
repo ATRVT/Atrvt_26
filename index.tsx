1f9bc9f6ee839bab527c44afdc8922ee67d5ee9c
ATRVT <raulmp242@gmail.com>
2025-12-11 08:15:05 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Asegúrate de tener este archivo o bórralo si usas solo Tailwind CDN
 
 const rootElement = document.getElementById('root');
 
